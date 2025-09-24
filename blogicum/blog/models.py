7d72e3a07c328d0f2b4ca759113de908db0c3869
bezbig <bezbig@yandex.ru>
2025-09-24 17:48:21 +0300

    Localization admin-zone

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -5,8 +5,8 @@ User = get_user_model()
 
 class PublishedModel(models.Model):
     """Абстрактная модель. Добвляет флаг is_published."""
-    is_published = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField('Опубликовано', default=True, help_text='Снимите галочку, чтобы скрыть публикацию')
+    created_at = models.DateTimeField('Добавлено',auto_now_add=True)
 
     class Meta:
         abstract = True
@@ -16,34 +16,47 @@ class PublishedModel(models.Model):
 
 class Post(PublishedModel):
     title = models.CharField('Заголовок', max_length=256)
-    text = models.TextField('Содержание')
-    pub_date = models.DateTimeField('Дата публикации')
+    text = models.TextField('Заголовок')
+    pub_date = models.DateTimeField(
+        'Дата и время публикации',
+        auto_now_add=True,
+        help_text='Если установить дату и время в будущем — '
+        'можно делать отложенные публикации.'
+        )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        verbose_name='Автор публикации',
     )
     location = models.ForeignKey(
         'Location',
         on_delete=models.SET_NULL,
+        verbose_name='Местоположение',
         null=True, blank=True
     )
     category = models.ForeignKey(
         'Category',
         on_delete=models.SET_NULL,
+        verbose_name='Категория',
         null=True,
     )
 
     class Meta:
-        verbose_name = 'пост'
-        verbose_name_plural = 'Посты'
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
 
     def __str__(self):
         return self.title
     
 class Category(PublishedModel):
-    title = models.CharField('Название', max_length=256)
+    title = models.CharField('Заголовок', max_length=256)
     description = models.TextField('Описание')
-    slug = models.SlugField('Слаг', unique=True)
+    slug = models.SlugField(
+        'Идентификатор',
+        unique=True,
+        help_text='Идентификатор страницы для URL;'
+        'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        )
 
     class Meta:
         verbose_name = 'категория'
@@ -53,11 +66,11 @@ class Category(PublishedModel):
         return self.name
     
 class Location(PublishedModel):
-    name = models.CharField('Название', max_length=256)
+    name = models.CharField('Название места', max_length=256)
 
     class Meta:
-        verbose_name = 'локация'
-        verbose_name_plural = 'Локации'
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
 
     def __str__(self):
         return self.name
